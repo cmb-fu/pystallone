d0f6742cd57fefc6087a5aec1afb7b32dfd55a65
marscher <m.scherer@fu-berlin.de>
2014-08-08 14:52:47 +0200

    added tests

---
+++ pystallone/tests/test_init_with_args.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestInitWithArgs(unittest.TestCase):
+    
+    def testWithArgsExtendClassPath(self):
+        import pystallone
+        args = ['-Xms64m', '-Djava.class.path=/dev/null']
+        jvm = None
+        
+        pystallone.startJVM(jvm, args)
+        
+        a = pystallone.API.doublesNew.array(10)
+        
+        self.assertEqual(10, a.size())
+    
+    def tearDown(self):
+        import pystallone
+        pystallone.shutdownJVM()
+        
