edd969c9108b5b142027fc9d0b6e99ae09c7687b
marscher <m.scherer@fu-berlin.de>
2014-08-11 14:23:57 +0200

    [setup] add numpy as requirement

--- setup.py
+++ setup.py
@@ -17,14 +17,15 @@ metadata = dict(
     package_data = {'pystallone' : ['stallone-current-jar-with-dependencies.jar',
                                     'include/jni.h',
                                     'include/jni_md.h']},
-    install_requires = ['JPype1 >= 0.5.5.4'],
+    install_requires = ['JPype1 >= 0.5.5.3',
+                        'numpy >= 1.6.0'],
 )
 
 # HACK for JPype installation:
 # we do not want the user to install a complete JDK, so we provide jni.h here.
 # NOTE: this temporarily overrides an existing set JAVA_HOME, if you want to avoid
 # this, comment this line! 
-os.environ['JAVA_HOME'] = os.path.abspath(os.path.join('pystallone','include'))
+os.environ['JAVA_HOME'] = os.path.abspath(os.path.join('pystallone', 'include'))
 
 
 setup(**metadata)
