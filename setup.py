de2bddcf1a9dbc99e70df29bc0524eebe42b0e49
marscher <m.scherer@fu-berlin.de>
2014-08-17 15:44:55 +0200

    added classifiers

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ import os
 import sys
 
 # support python 2 and 3
-jpype_species = 'JPype1>=0.5.5.3' if sys.version_info[0] == 2 else 'JPype1-py3>=0.5.5.2'
+jpype_species = 'JPype1>=0.5.5.4' if sys.version_info[0] == 2 else 'JPype1-py3>=0.5.5.2'
 
 metadata = dict(
     name = 'pystallone',
     version = '1.0-SNAPSHOT',
     # TODO: add url
     url = '',
-    author = 'Frank Noe, Martin Scherer',
-    maintainer = 'Martin Scherer',
+    author = 'Frank Noe, Martin K. Scherer',
+    maintainer = 'Martin K. Scherer',
     author_email = 'stallone@lists.fu-berlin.de',
     packages = ['pystallone'],
     package_data = {'pystallone' : ['stallone-current-jar-with-dependencies.jar',
@@ -22,6 +22,25 @@ metadata = dict(
                                     'include/jni_md.h']},
     install_requires = [jpype_species,
                         'numpy >= 1.6.0'],
+                
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Java',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries :: Java Libraries'
+    ],
 )
 
 # HACK for JPype installation:
